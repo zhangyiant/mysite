3506c97dc94833c70f79bf5088232554186eb6a7
Yi Zhang <zhangyiant@hotmail.com>
2019-03-24 11:48:23 +0800

    Add PhoneNumber admin.

--- mysite/myfirstapp/admin.py
+++ mysite/myfirstapp/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from .models import Contact
+from .models import Contact, PhoneNumber
 
 # Register your models here.
 
 admin.site.register(Contact)
+admin.site.register(PhoneNumber)
