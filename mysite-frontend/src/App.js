026450329e59c21952f5f12c4b681c9dd3cbe5e5
Yi Zhang <zhangyiant@hotmail.com>
2019-03-25 21:10:54 +0800

    Add React Router.

--- mysite-frontend/src/App.js
+++ mysite-frontend/src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import './App.css';
 import axios from 'axios';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
 
 class ContactRow extends Component {
     render() {
@@ -47,12 +49,22 @@ class Contacts extends Component {
     }
 }
 
+class MyTestPage extends Component {
+    render() {
+        return (
+            <div>
+                HelloWorld!
+            </div>);
+    }
+}
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Contacts />
-      </div>
+        <Router>
+            <Route path="/" exact component={ Contacts } />
+            <Route path="/mypage" component={ MyTestPage } />
+        </Router>
     );
   }
 }
