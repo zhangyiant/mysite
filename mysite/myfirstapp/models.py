229b93be5d85b737c6c67df5ee3afe1070b347dc
Yi Zhang <zhangyiant@hotmail.com>
2019-04-14 20:01:49 +0800

    table name override.

--- mysite/myfirstapp/models.py
+++ mysite/myfirstapp/models.py
@@ -9,3 +9,11 @@ class PhoneNumber(models.Model):
     contact = models.ForeignKey(Contact, on_delete=models.CASCADE)
     number = models.CharField(max_length=20)
 
+class AutoTableName(models.Model):
+    my_field = models.CharField(max_length=200)
+    
+class MyTableName(models.Model):
+    class Meta:
+        db_table = "my_abc_table"
+    my_field = models.CharField(max_length=200)
+
