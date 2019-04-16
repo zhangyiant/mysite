212b0bd319da18029481509110fcdd3cf7abbc75
Yi Zhang <zhangyiant@hotmail.com>
2019-04-16 21:47:45 +0800

    Appconfig.

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -31,7 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'myfirstapp.apps.MyfirstappConfig',
+    #'myfirstapp.apps.MyfirstappConfig',
+    #'myfirstapp',
+    'appconfigs.MyNewFirstAppConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
