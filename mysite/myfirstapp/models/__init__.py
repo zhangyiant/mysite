0504bf9b50a9af5d1d5a4ea8a7a7ba7528ca32af
Yi Zhang <zhangyiant@hotmail.com>
2019-04-25 21:59:53 +0800

    split models.py

---
+++ mysite/myfirstapp/models/__init__.py
@@ -0,0 +1,3 @@
+from .app_models import *
+from .field_test_models import *
+
--- mysite/myfirstapp/models/field_test_models.py
+++ mysite/myfirstapp/models/field_test_models.py
@@ -1,15 +1,6 @@
 from datetime import datetime
 from django.db import models
 
-# Create your models here.
-class Contact(models.Model):
-    name = models.CharField(max_length=200)
-    gender = models.CharField(max_length=10)
-
-class PhoneNumber(models.Model):
-    contact = models.ForeignKey(Contact, on_delete=models.CASCADE)
-    number = models.CharField(max_length=20)
-
 class AutoTableName(models.Model):
     my_field = models.CharField(max_length=200)
     
